66f51f9cd5fe7e1bb845b0db29e91ca4fec98eda
itbank18 <itbank18@DESKTOP-I8NDMJS>
2023-10-12 15:09:35 +0900

    commit

--- day01/src/variable/Quiz02.java
+++ day01/src/variable/Quiz02.java
@@ -13,20 +13,27 @@ public class Quiz02 {
 		//자신의 나이를 정수형 변수에 저장하세요
 		
 		int age = 27;
-		
+		Integer age2 = 27;
 		
 		// 새로운 실수형에 0을 대입하세요
 		
-		double db = 0;
+		double db = 0;  // 우변의 0은 자료형이 명시되지 않은 리터럴이지만, 리터럴 정수의 기본자료형은 int이다
+		//Double db2 = 0; //Type mismatch: cannot convert from int to Double
+		//Double db2 = 0.0;
+		//Double db2 = (double)0;
+	    Double db2 = Double.valueOf(0);
+		
+		//Double db2 = (double)(int)0;
+	
 		
 		// 방금 만든 실수형 변수에 3.14를 대입하세요
 		
 		 db = 3.14;
-		
+		 db2 = 3.14; // 실수의 대표자형은 double이므로, 리터럴 값도 double타입을 가지게 된다.
 		// 지금까지 만든 3개 변수에 담긴 값을 각각 한줄씩 화면에 출력하세요
 		
-		System.out.println("나이 :" +age);
-		System.out.println(db);
+		System.out.println(name);
+		System.out.println(age);
 		System.out.println(db);
 		
 		// (primitive를 이용해서 한번, WrapperClass를 이용해서 한번 코드를 실행하세요
@@ -35,8 +42,8 @@ public class Quiz02 {
 		System.out.println((double)db);
 		
 		
-		System.out.println((Integer)age);
-		System.out.println((Double)db);
+		System.out.println((Integer)age2);
+		System.out.println((Double)db2);
 		System.out.println((Double)db);
 		
 		
